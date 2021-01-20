6841ccc4677db84155ec83429d28c2c596595a21
Sayful Islam <sayful.islam001@gmail.com>
2021-01-20 13:18:13 +0530

    Fix card upload title typo fix.

--- assets/src/frontend/designers/components/CardUploaderModal.vue
+++ assets/src/frontend/designers/components/CardUploaderModal.vue
@@ -7,7 +7,7 @@
 					<text-field type="textarea" v-model="card.title" label="Title"
 								:has-error="!!errors.title"
 								:validation-text="errors.title?errors.title[0]:''"
-								help-text="Write card title. Cart title will be used as product title."
+								help-text="Write card title. Card title will be used as product title."
 								:rows="1"
 					/>
 				</column>
@@ -43,23 +43,23 @@
 						<shapla-switch v-model="has_suggest_tags" true-value="yes" false-value="no"
 									   label="Suggest a new tag."/>
 						<text-field
-								v-if="has_suggest_tags === 'yes'"
-								label="Tags"
-								type="textarea"
-								help-text="Write your suggested tags, separate by comma if you have multiple suggestion"
-								v-model="card.suggest_tags"
+							v-if="has_suggest_tags === 'yes'"
+							label="Tags"
+							type="textarea"
+							help-text="Write your suggested tags, separate by comma if you have multiple suggestion"
+							v-model="card.suggest_tags"
 						/>
 					</div>
 				</column>
 				<column :tablet="6" v-for="attribute in card_attributes" :key="attribute.attribute_name"
 						v-if="card_attributes.length">
 					<select-field
-							v-model="card.attributes[attribute.attribute_name]"
-							:options="attribute.options"
-							:label="attribute.attribute_label"
-							label-key="name" value-key="id" multiple :searchable="attribute.options.length > 5"
-							:has-error="!!(errors.attributes && errors.attributes[attribute.attribute_name])"
-							:validation-text="errors.attributes?errors.attributes[0]:''"
+						v-model="card.attributes[attribute.attribute_name]"
+						:options="attribute.options"
+						:label="attribute.attribute_label"
+						label-key="name" value-key="id" multiple :searchable="attribute.options.length > 5"
+						:has-error="!!(errors.attributes && errors.attributes[attribute.attribute_name])"
+						:validation-text="errors.attributes?errors.attributes[0]:''"
 					/>
 				</column>
 				<column :tablet="12">
@@ -73,10 +73,10 @@
 						<columns multiline>
 							<column :tablet="2" v-for="marketPlace in market_places" :key="marketPlace.key">
 								<shapla-switch
-										:value="marketPlace.key"
-										v-model="card.market_places"
-										@change="handleMarketPlaceChange"
-										:readonly="marketPlace.key === 'yousaidit'"
+									:value="marketPlace.key"
+									v-model="card.market_places"
+									@change="handleMarketPlaceChange"
+									:readonly="marketPlace.key === 'yousaidit'"
 								>
 									<img :src="marketPlace.logo" :alt="marketPlace.key">
 								</shapla-switch>
@@ -92,11 +92,11 @@
 						<toggle :name="`Upload files for size: ${getHeaderText(size)}`" :key="`upload-${size}`"
 								v-for="(size, index) in card.sizes" :selected="index === 0">
 							<file-uploader
-									:url="attachment_upload_url"
-									@before:send="addAdditionalData"
-									:params="{type:'card_pdf',card_size:size}"
-									@success="handlePdfUpload"
-									:input-id="`yousaiditcard-pdf-input-${size}`"
+								:url="attachment_upload_url"
+								@before:send="addAdditionalData"
+								:params="{type:'card_pdf',card_size:size}"
+								@success="handlePdfUpload"
+								:input-id="`yousaiditcard-pdf-input-${size}`"
 							/>
 						</toggle>
 					</toggles>
@@ -105,12 +105,12 @@
 
 			<columns multiline v-if="current_step === 3">
 				<column :tablet="3"><strong>Title</strong></column>
-				<column :tablet="9">{{card.title}}</column>
+				<column :tablet="9">{{ card.title }}</column>
 
 				<column :tablet="3"><strong>Card Sizes</strong></column>
 				<column :tablet="9">
 					<shapla-chip v-for="_size in card_sizes" v-if="card.sizes.indexOf(_size.value) !== -1"
-								 :key="_size.value"> {{_size.label}}
+								 :key="_size.value"> {{ _size.label }}
 					</shapla-chip>
 				</column>
 
@@ -118,22 +118,22 @@
 				<column :tablet="9">
 					<shapla-chip v-for="_cat in card_categories"
 								 v-if="card.categories_ids.indexOf(_cat.id.toString()) !== -1" :key="_cat.id">
-						{{_cat.name}}
+						{{ _cat.name }}
 					</shapla-chip>
 				</column>
 
 				<column :tablet="3"><strong>Card Tags</strong></column>
 				<column :tablet="9">
 					<shapla-chip v-for="_tag in card_tags" v-if="card.tags_ids.indexOf(_tag.id.toString()) !== -1"
-								 :key="_tag.id"> {{_tag.name}}
+								 :key="_tag.id"> {{ _tag.name }}
 					</shapla-chip>
 				</column>
 
 				<template v-for="_attr in card_attributes">
-					<column :tablet="3"><strong>{{_attr.attribute_label}}</strong></column>
+					<column :tablet="3"><strong>{{ _attr.attribute_label }}</strong></column>
 					<column :tablet="9">
 						<shapla-chip v-for="_tag in _attr.options" v-if="isAttributeSelected(_attr,_tag)"
-									 :key="_tag.id"> {{_tag.name}}
+									 :key="_tag.id"> {{ _tag.name }}
 						</shapla-chip>
 					</column>
 				</template>
@@ -142,9 +142,9 @@
 					<column :tablet="3"><strong>Card Image</strong></column>
 					<column :tablet="9">
 						<pdf-image-item
-								:is-multiple="false"
-								:images="card_image.full"
-								url-key="src"
+							:is-multiple="false"
+							:images="card_image.full"
+							url-key="src"
 						/>
 					</column>
 				</template>
@@ -153,8 +153,8 @@
 					<column :tablet="3"><strong>Card Gallery Images</strong></column>
 					<column :tablet="9">
 						<pdf-image-item
-								:is-multiple="true"
-								:images="get_formatted_card_images"
+							:is-multiple="true"
+							:images="get_formatted_card_images"
 						/>
 					</column>
 				</template>
@@ -164,11 +164,11 @@
 						<column :tablet="3"><strong>Card PDFs</strong></column>
 						<column :tablet="9">
 							<pdf-card-item
-									v-for="(pdf_data,size_slug) in pdf_files"
-									:key="size_slug"
-									:header-text="getHeaderText(size_slug)"
-									:items="pdf_data"
-									url-key="attachment_url"
+								v-for="(pdf_data,size_slug) in pdf_files"
+								:key="size_slug"
+								:header-text="getHeaderText(size_slug)"
+								:items="pdf_data"
+								url-key="attachment_url"
 							/>
 						</column>
 					</columns>
@@ -181,20 +181,20 @@
 					<toggles>
 						<toggle name="Card image" selected>
 							<file-uploader
-									:url="attachment_upload_url"
-									@before:send="addAdditionalData"
-									:params="{type:'card_image'}"
-									@success="handleCardImageUpload"
-									input-id="yousaiditcard-image-upload-input"
+								:url="attachment_upload_url"
+								@before:send="addAdditionalData"
+								:params="{type:'card_image'}"
+								@success="handleCardImageUpload"
+								input-id="yousaiditcard-image-upload-input"
 							/>
 						</toggle>
 						<toggle name="Card gallery images (optional)">
 							<file-uploader
-									:url="attachment_upload_url"
-									@before:send="addAdditionalData"
-									:params="{type:'card_gallery_images'}"
-									@success="handleCardGalleryImagesUpload"
-									input-id="yousaiditcard-gallery-image-upload-input"
+								:url="attachment_upload_url"
+								@before:send="addAdditionalData"
+								:params="{type:'card_gallery_images'}"
+								@success="handleCardGalleryImagesUpload"
+								input-id="yousaiditcard-gallery-image-upload-input"
 							/>
 						</toggle>
 					</toggles>
@@ -214,204 +214,201 @@
 </template>
 
 <script>
-	import axios from 'axios'
-	import textField from "shapla-text-field";
-	import selectField from 'shapla-select-field';
-	import shaplaButton from 'shapla-button';
-	import modal from 'shapla-modal'
-	import {columns, column} from 'shapla-columns';
-	import FileUploader from 'shapla-file-uploader';
-	import {toggles, toggle} from 'shapla-toggles';
-	import DesignerEventBus from "./DesignerEventBus";
-	import imageContainer from 'shapla-image-container';
-	import shaplaSwitch from 'shapla-switch';
-	import shaplaChip from 'shapla-chip';
-	import PdfImageItem from "../../../components/PdfImageItem";
-	import PdfCardItem from "../../../components/PdfCardItem";
+import axios from 'axios'
+import textField from "shapla-text-field";
+import selectField from 'shapla-select-field';
+import shaplaButton from 'shapla-button';
+import modal from 'shapla-modal'
+import {column, columns} from 'shapla-columns';
+import FileUploader from 'shapla-file-uploader';
+import {toggle, toggles} from 'shapla-toggles';
+import DesignerEventBus from "./DesignerEventBus";
+import imageContainer from 'shapla-image-container';
+import shaplaSwitch from 'shapla-switch';
+import shaplaChip from 'shapla-chip';
+import PdfImageItem from "../../../components/PdfImageItem";
+import PdfCardItem from "../../../components/PdfCardItem";
 
-	export default {
-		name: "CardUploaderModal",
-		components: {
-			PdfImageItem, textField, selectField, shaplaButton, modal, columns, column, FileUploader,
-			toggles, toggle, imageContainer, shaplaSwitch, PdfCardItem, shaplaChip
-		},
-		props: {
-			active: {type: Boolean, default: false},
-			card_sizes: {type: Array, default: () => []},
-			card_categories: {type: Array, default: () => []},
-			card_tags: {type: Array, default: () => []},
-			card_attributes: {type: Array, default: () => []},
-			market_places: {type: Array, default: () => []},
-		},
-		data() {
-			return {
-				card: {
-					title: '',
-					sizes: [],
-					categories_ids: [],
-					tags_ids: [],
-					attributes: {},
-					image_id: 0,
-					gallery_images_ids: [],
-					market_places: ['yousaidit'],
-					pdf_ids: {},
-					rude_card: 'no',
-					suggest_tags: '',
-				},
-				pdf_files: {},
-				card_images: [],
-				card_image: {},
-				errors: {},
-				current_step: 1,
-				has_suggest_tags: 'no',
+export default {
+	name: "CardUploaderModal",
+	components: {
+		PdfImageItem, textField, selectField, shaplaButton, modal, columns, column, FileUploader,
+		toggles, toggle, imageContainer, shaplaSwitch, PdfCardItem, shaplaChip
+	},
+	props: {
+		active: {type: Boolean, default: false},
+		card_sizes: {type: Array, default: () => []},
+		card_categories: {type: Array, default: () => []},
+		card_tags: {type: Array, default: () => []},
+		card_attributes: {type: Array, default: () => []},
+		market_places: {type: Array, default: () => []},
+	},
+	data() {
+		return {
+			card: {
+				title: '',
+				sizes: [],
+				categories_ids: [],
+				tags_ids: [],
+				attributes: {},
+				image_id: 0,
+				gallery_images_ids: [],
+				market_places: ['yousaidit'],
+				pdf_ids: {},
+				rude_card: 'no',
+				suggest_tags: '',
+			},
+			pdf_files: {},
+			card_images: [],
+			card_image: {},
+			errors: {},
+			current_step: 1,
+			has_suggest_tags: 'no',
+		}
+	},
+	computed: {
+		can_go_next_step() {
+			if (this.current_step === 1) {
+				if (this.card_attributes.length && !Object.keys(this.card.attributes).length) {
+					return false;
+				}
+				return !!(this.card.title.length > 5 && this.card.sizes.length && this.card.categories_ids.length);
 			}
+			if (this.current_step === 2) {
+				return this.num_of_pdf_files === this.card.sizes.length;
+			}
+			// if (this.current_step === 2) {
+			// 	return !!this.card.image_id;
+			// }
+			return false;
 		},
-		computed: {
-			can_go_next_step() {
-				if (this.current_step === 1) {
-					if (this.card_attributes.length && !Object.keys(this.card.attributes).length) {
-						return false;
-					}
-					return !!(
-							this.card.title.length > 5 && this.card.sizes.length &&
-							this.card.categories_ids.length && this.card.tags_ids.length
-					);
+		num_of_pdf_files() {
+			let count = 0;
+			for (let [key, value] of Object.entries(this.pdf_files)) {
+				count += Array.isArray(value) ? 1 : 0;
+			}
+			return count;
+		},
+		designer_id() {
+			return DesignerProfile.user.id;
+		},
+		attachment_upload_url() {
+			return DesignerProfile.restRoot + '/designers/' + this.designer_id + '/attachment';
+		},
+		modalTitle() {
+			if (2 === this.current_step) {
+				return 'Add card preview images'
+			}
+			if (3 === this.current_step) {
+				return 'Add card PDF files'
+			}
+			if (4 === this.current_step) {
+				return 'Preview'
+			}
+			return 'Add card detail';
+		},
+		get_formatted_card_images() {
+			if (this.card_images.length < 1) {
+				return [];
+			}
+			return this.card_images.map(img => {
+				return {
+					url: img.full.src,
+					width: img.full.width,
+					height: img.full.height
 				}
-				if (this.current_step === 2) {
-					return this.num_of_pdf_files === this.card.sizes.length;
-				}
-				// if (this.current_step === 2) {
-				// 	return !!this.card.image_id;
-				// }
-				return false;
-			},
-			num_of_pdf_files() {
-				let count = 0;
-				for (let [key, value] of Object.entries(this.pdf_files)) {
-					count += Array.isArray(value) ? 1 : 0;
-				}
-				return count;
-			},
-			designer_id() {
-				return DesignerProfile.user.id;
-			},
-			attachment_upload_url() {
-				return DesignerProfile.restRoot + '/designers/' + this.designer_id + '/attachment';
-			},
-			modalTitle() {
-				if (2 === this.current_step) {
-					return 'Add card preview images'
-				}
-				if (3 === this.current_step) {
-					return 'Add card PDF files'
-				}
-				if (4 === this.current_step) {
-					return 'Preview'
-				}
-				return 'Add card detail';
-			},
-			get_formatted_card_images() {
-				if (this.card_images.length < 1) {
-					return [];
-				}
-				return this.card_images.map(img => {
-					return {
-						url: img.full.src,
-						width: img.full.width,
-						height: img.full.height
-					}
+			});
+		}
+	},
+	methods: {
+		handleMarketPlaceChange(value) {
+			console.log(value);
+		},
+		isAttributeSelected(attribute, term) {
+			if (this.card.attributes[attribute.attribute_name]) {
+				return this.card.attributes[attribute.attribute_name].indexOf(term.id.toString()) !== -1;
+			}
+			return false;
+		},
+		closeModal() {
+			this.$emit('close');
+		},
+		handleSubmit() {
+			this.errors = {};
+			DesignerEventBus.$emit('loading', true);
+			axios.post(DesignerProfile.restRoot + '/designers/' + this.designer_id + '/cards', this.card).then(response => {
+				this.closeModal();
+				DesignerEventBus.$emit('loading', false);
+				DesignerEventBus.$emit('notify', {
+					title: 'Success',
+					message: 'Card has been submitted successfully.',
+					type: 'success'
 				});
+				this.card = {
+					title: '', sizes: [], categories_ids: [], tags_ids: [], attributes: {}, image_id: 0,
+					gallery_images_ids: [], pdf_ids: {}, rude_card: 'no',
+				};
+				this.current_step = 1;
+				DesignerEventBus.$emit('card:added', response.data.data);
+			}).catch(error => {
+				DesignerEventBus.$emit('loading', false);
+				if (error.response.data.errors) {
+					this.errors = error.response.data.errors;
+				}
+			})
+		},
+		handleCardImageUpload(fileObject, serverResponse) {
+			this.card.image_id = serverResponse.data.attachment.id;
+			this.card_image = serverResponse.data.attachment;
+		},
+		handleCardGalleryImagesUpload(fileObject, serverResponse) {
+			this.card.gallery_images_ids.push(serverResponse.data.attachment.id);
+			this.card_images.push(serverResponse.data.attachment);
+		},
+		handlePdfUpload(fileObject, serverResponse) {
+			let key = serverResponse.data.query.card_size, pdf_ids = {}, pdf_files = {};
+
+			if (this.card.pdf_ids[key]) {
+				this.card.pdf_ids[key].push(serverResponse.data.attachment.id);
+			} else {
+				pdf_ids[key] = [serverResponse.data.attachment.id];
+				this.card.pdf_ids = Object.assign({}, this.card.pdf_ids, pdf_ids);
+			}
+
+			if (this.pdf_files[key]) {
+				this.pdf_files[key].push(serverResponse.data.attachment);
+			} else {
+				pdf_files[key] = [serverResponse.data.attachment];
+				this.pdf_files = Object.assign({}, this.pdf_files, pdf_files);
 			}
 		},
-		methods: {
-			handleMarketPlaceChange(value) {
-				console.log(value);
-			},
-			isAttributeSelected(attribute, term) {
-				if (this.card.attributes[attribute.attribute_name]) {
-					return this.card.attributes[attribute.attribute_name].indexOf(term.id.toString()) !== -1;
-				}
-				return false;
-			},
-			closeModal() {
-				this.$emit('close');
-			},
-			handleSubmit() {
-				this.errors = {};
-				DesignerEventBus.$emit('loading', true);
-				axios.post(DesignerProfile.restRoot + '/designers/' + this.designer_id + '/cards', this.card).then(response => {
-					this.closeModal();
-					DesignerEventBus.$emit('loading', false);
-					DesignerEventBus.$emit('notify', {
-						title: 'Success',
-						message: 'Card has been submitted successfully.',
-						type: 'success'
-					});
-					this.card = {
-						title: '', sizes: [], categories_ids: [], tags_ids: [], attributes: {}, image_id: 0,
-						gallery_images_ids: [], pdf_ids: {}, rude_card: 'no',
-					};
-					this.current_step = 1;
-					DesignerEventBus.$emit('card:added', response.data.data);
-				}).catch(error => {
-					DesignerEventBus.$emit('loading', false);
-					if (error.response.data.errors) {
-						this.errors = error.response.data.errors;
-					}
-				})
-			},
-			handleCardImageUpload(fileObject, serverResponse) {
-				this.card.image_id = serverResponse.data.attachment.id;
-				this.card_image = serverResponse.data.attachment;
-			},
-			handleCardGalleryImagesUpload(fileObject, serverResponse) {
-				this.card.gallery_images_ids.push(serverResponse.data.attachment.id);
-				this.card_images.push(serverResponse.data.attachment);
-			},
-			handlePdfUpload(fileObject, serverResponse) {
-				let key = serverResponse.data.query.card_size, pdf_ids = {}, pdf_files = {};
-
-				if (this.card.pdf_ids[key]) {
-					this.card.pdf_ids[key].push(serverResponse.data.attachment.id);
-				} else {
-					pdf_ids[key] = [serverResponse.data.attachment.id];
-					this.card.pdf_ids = Object.assign({}, this.card.pdf_ids, pdf_ids);
-				}
-
-				if (this.pdf_files[key]) {
-					this.pdf_files[key].push(serverResponse.data.attachment);
-				} else {
-					pdf_files[key] = [serverResponse.data.attachment];
-					this.pdf_files = Object.assign({}, this.pdf_files, pdf_files);
-				}
-			},
-			addAdditionalData(xhr) {
-				xhr.setRequestHeader('X-WP-Nonce', window.DesignerProfile.restNonce);
-			},
-			getHeaderText(size_slug) {
-				let item = this.card_sizes.find(size => size.value === size_slug);
-				return item.label;
-			}
+		addAdditionalData(xhr) {
+			xhr.setRequestHeader('X-WP-Nonce', window.DesignerProfile.restNonce);
+		},
+		getHeaderText(size_slug) {
+			let item = this.card_sizes.find(size => size.value === size_slug);
+			return item.label;
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	.yousaidit-designer-profile-card-modal {
-		.shapla-chip:not(:last-child) {
-			margin-right: 5px;
-		}
-
-		h3.section-title {
-			color: var(--shapla-primary);
-			font-size: 1.25rem;
-			font-weight: 400;
-			text-transform: uppercase;
-		}
-
-		h4.section-subtitle {
-			font-size: .75rem;
-			font-weight: 400;
-		}
+.yousaidit-designer-profile-card-modal {
+	.shapla-chip:not(:last-child) {
+		margin-right: 5px;
 	}
+
+	h3.section-title {
+		color: var(--shapla-primary);
+		font-size: 1.25rem;
+		font-weight: 400;
+		text-transform: uppercase;
+	}
+
+	h4.section-subtitle {
+		font-size: .75rem;
+		font-weight: 400;
+	}
+}
 </style>
