39e2f186d582f3f883fc54d9ef3223cc4eb82949
Sayful Islam <sayful.islam001@gmail.com>
2024-04-04 10:49:36 +0600

    Fix image upload issue on profile page.

--- .vue3/resources/frontend-designer-dashboard/pages/Profile.vue
+++ .vue3/resources/frontend-designer-dashboard/pages/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="yousaidit-designer-profile">
-    <designer-profile-header
+    <DesignerProfileHeader
         :designer-name="store.designer.display_name"
         :designer-location="store.designer.location"
         :designer-bio="store.designer.description"
@@ -12,78 +12,78 @@
     <p>&nbsp;</p>
     <ShaplaTabs fullwidth tab-style="toggle">
       <ShaplaTab name="Personal Detail" selected>
-        <profile-field title="Name" :content="`${store.designer.first_name} ${store.designer.last_name}`"
+        <ProfileField title="Name" :content="`${store.designer.first_name} ${store.designer.last_name}`"
                        @save="updateName">
           <ShaplaInput type="text" label="First Name" v-model="store.designer.first_name"/>
           <ShaplaInput type="text" label="Last Name" v-model="store.designer.last_name"/>
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="Display Name" :content="store.designer.display_name" @save="updateDisplayName">
+        <ProfileField title="Display Name" :content="store.designer.display_name" @save="updateDisplayName">
           <ShaplaSelect
               label="Display Name"
               v-model="store.designer.display_name"
               :options="store.displayNameOptions"
           />
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="Location" :content="store.designer.location" @save="updateLocation">
+        <ProfileField title="Location" :content="store.designer.location" @save="updateLocation">
           <ShaplaInput type="text" label="Location" v-model="store.designer.location"/>
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="Password" content="**********" @save="updatePassword">
+        <ProfileField title="Password" content="**********" @save="updatePassword">
           <ShaplaInput type="password" label="Current Password" v-model="store.current_password"/>
           <ShaplaInput type="password" label="New Password" v-model="store.new_password"/>
           <ShaplaInput type="password" label="Confirm Password" v-model="store.confirm_password"/>
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="About Yourself" :content="store.designer.description" field-width="500px"
+        <ProfileField title="About Yourself" :content="store.designer.description" field-width="500px"
                        @save="updateDescription">
           <ShaplaInput type="textarea" label="Detail" v-model="store.designer.description"/>
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="Profile URL" :content="`${store.designer.profile_base_url}/${store.designer.user_login}`"
+        <ProfileField title="Profile URL" :content="`${store.designer.profile_base_url}/${store.designer.user_login}`"
                        @save="store.updateProfileUrl">
           <span v-html="`${store.designer.profile_base_url}/${store.designer.user_login}`"></span>
           <ShaplaInput type="url" label="Username" v-model="store.designer.user_login"
-                      help-text="Your username will be used as your profile page URL."/>
-        </profile-field>
+                       help-text="Your username will be used as your profile page URL."/>
+        </ProfileField>
 
-        <profile-field title="Instagram Profile" :content="store.designer.instagram_url" field-width="500px"
+        <ProfileField title="Instagram Profile" :content="store.designer.instagram_url" field-width="500px"
                        @save="updateInstagramUrl">
           <ShaplaInput type="url" label="URL" v-model="store.designer.instagram_url"/>
-        </profile-field>
+        </ProfileField>
       </ShaplaTab>
 
       <ShaplaTab name="Business Detail">
-        <profile-field title="Business Name" :content="store.designer.business_name" @save="updateBusinessName">
+        <ProfileField title="Business Name" :content="store.designer.business_name" @save="updateBusinessName">
           <ShaplaInput type="text" label="Business Name" v-model="store.designer.business_name"/>
-        </profile-field>
+        </ProfileField>
 
-        <profile-field title="Business Address" @save="updateBusinessAddress"
+        <ProfileField title="Business Address" @save="updateBusinessAddress"
                        :content="store.designer.formatted_address">
           <ShaplaInput type="text" label="Address Line 1" autocomplete="address-line1"
-                      v-model="store.designer.business_address.address_1" name="address_1"/>
+                       v-model="store.designer.business_address.address_1" name="address_1"/>
           <ShaplaInput type="text" label="Address Line 2" autocomplete="address-line2"
-                      v-model="store.designer.business_address.address_2" name="address_2"/>
+                       v-model="store.designer.business_address.address_2" name="address_2"/>
           <ShaplaInput type="text" label="City" autocomplete="address-level2"
-                      v-model="store.designer.business_address.city" name="city"/>
+                       v-model="store.designer.business_address.city" name="city"/>
           <ShaplaInput type="text" label="Post Code" autocomplete="postal-code"
-                      v-model="store.designer.business_address.post_code" name="post_code"/>
+                       v-model="store.designer.business_address.post_code" name="post_code"/>
           <ShaplaInput type="text" label="Country" autocomplete="country"
-                      v-model="store.designer.business_address.country" name="country"/>
-        </profile-field>
+                       v-model="store.designer.business_address.country" name="country"/>
+        </ProfileField>
 
-        <profile-field title="VAT" :content="store.designer.vat_registration_number" @save="updateVatInfo">
+        <ProfileField title="VAT" :content="store.designer.vat_registration_number" @save="updateVatInfo">
           <ShaplaInput type="text" label="Vat Registration Number" v-model="store.designer.vat_registration_number"/>
           <ShaplaInput type="date" label="Vat Certificate Issue Date"
-                      v-model="store.designer.vat_certificate_issue_date"/>
-        </profile-field>
+                       v-model="store.designer.vat_certificate_issue_date"/>
+        </ProfileField>
       </ShaplaTab>
 
       <ShaplaTab name="Payouts">
-        <profile-field title="PayPal Email Address" :content="store.designer.paypal_email" @save="updatePayPalEmail">
+        <ProfileField title="PayPal Email Address" :content="store.designer.paypal_email" @save="updatePayPalEmail">
           <ShaplaInput type="email" label="Email Address" v-model="store.designer.paypal_email"/>
-        </profile-field>
+        </ProfileField>
       </ShaplaTab>
 
       <ShaplaTab name="Card Settings">
@@ -108,7 +108,8 @@
         src-key="attachment_url"
         @close="state.showChangeProfileModal = false"
         @select:image="handleChooseProfileImage"
-        @before:send="addNonceHeaderForProfileImage"
+        :headers="{'X-WP-Nonce': restNonce}"
+        :params="{'type': 'avatar'}"
         @success="(file,response)=>refreshMediaList(response,'avatar')"
     />
     <ShaplaMediaModal
@@ -118,9 +119,10 @@
         :url="store.uploadUrl"
         primary-key="id"
         src-key="attachment_url"
+        :headers="{'X-WP-Nonce': restNonce}"
+        :params="{'type': 'cover'}"
         @close="state.showChangeCoverModal = false"
         @select:image="handleChooseCoverImage"
-        @before:send="addNonceHeaderForCover"
         @success="(file,response)=>refreshMediaList(response,'cover')"
     />
     <ShaplaMediaModal
@@ -131,22 +133,16 @@
         :url="store.uploadUrl"
         primary-key="id"
         src-key="attachment_url"
+        :headers="{'X-WP-Nonce': restNonce}"
+        :params="{'type': 'card-logo'}"
         @select:image="handleCardLogoImageId"
-        @before:send="addNonceHeaderForCardLogo"
         @success="(file,response)=>refreshMediaList(response,'card-logo')"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import {
-  ShaplaButton,
-  ShaplaInput,
-  ShaplaMediaModal,
-  ShaplaSelect,
-  ShaplaTab,
-  ShaplaTabs
-} from '@shapla/vue-components'
+import {ShaplaButton, ShaplaInput, ShaplaMediaModal, ShaplaSelect, ShaplaTab, ShaplaTabs} from '@shapla/vue-components'
 import ProfileField from "../components/ProfileField.vue";
 import DesignerProfileHeader from "../components/DesignerProfileHeader.vue";
 import useDesignerProfileStore from "../stores/store-profile.ts";
@@ -159,7 +155,9 @@ const state = reactive({
   showCardLogoModal: false,
 })
 
-onMounted(()=>{
+const restNonce = window.DesignerProfile.restNonce;
+
+onMounted(() => {
   store.getUserData();
   store.getUserUploadedImages();
 })
@@ -186,25 +184,14 @@ const handleChooseProfileImage = (image) => {
 const handleCardLogoImageId = (image) => {
   store.update({card_logo_id: image.id});
 }
-const handleChooseCoverImage =(image) => {
+const handleChooseCoverImage = (image) => {
   store.update({cover_photo_id: image.id});
 }
-const addNonceHeaderForCardLogo =(xhr, formData) => {
-  xhr.setRequestHeader('X-WP-Nonce', window.DesignerProfile.restNonce);
-  formData.append('type', 'card-logo');
-}
-const addNonceHeaderForCover =(xhr, formData) => {
-  xhr.setRequestHeader('X-WP-Nonce', window.DesignerProfile.restNonce);
-  formData.append('type', 'cover');
-}
-const addNonceHeaderForProfileImage = (xhr, formData) => {
-  xhr.setRequestHeader('X-WP-Nonce', window.DesignerProfile.restNonce);
-  formData.append('type', 'avatar');
-}
+
 const updateName = () => {
   store.update({first_name: store.designer.first_name, last_name: store.designer.last_name});
 }
-const updateDisplayName  = () => {
+const updateDisplayName = () => {
   store.update({display_name: store.designer.display_name});
 }
 const updateLocation = () => {
